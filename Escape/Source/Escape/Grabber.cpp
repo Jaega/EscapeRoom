061f0ed211f66669d00cc15a5e42202b1c984f47
Xinzhao Li <szxzlee@gmail.com>
2019-03-30 00:04:11 -0400

    Find input component and bind input action

--- Escape/Source/Escape/Grabber.cpp
+++ Escape/Source/Escape/Grabber.cpp
@@ -25,10 +25,27 @@ void UGrabber::BeginPlay()
 	if(PhysicsHandle){
 		
 	} else {
-		UE_LOG(LogTemp, Error, TEXT("%s  missing yhysics handle component!"), *(GetOwner()->GetName()));
+		UE_LOG(LogTemp, Error, TEXT("%s  missing physics handle component!"), *(GetOwner()->GetName()));
+	}
+	// Look for attached input component (only at run time)
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	if(PhysicsHandle){
+		UE_LOG(LogTemp, Warning, TEXT("Input component found!"));
+		// Bind input axis
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
+	} else {
+		UE_LOG(LogTemp, Error, TEXT("%s  missing input component!"), *(GetOwner()->GetName()));
 	}
 }
 
+void UGrabber::Grab(){
+	UE_LOG(LogTemp, Warning, TEXT("Grab pressed!"));
+}
+
+void UGrabber::Release(){
+	UE_LOG(LogTemp, Warning, TEXT("Grab released!"));
+}
 
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
--- Escape/Source/Escape/Grabber.h
+++ Escape/Source/Escape/Grabber.h
@@ -6,6 +6,7 @@
 #include "Components/ActorComponent.h"
 #include "DrawDebugHelpers.h"
 #include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Components/InputComponent.h"
 #include "Grabber.generated.h"
 
 
@@ -29,5 +30,9 @@ public:
 private:
 	float Reach = 100.f;
 	UPhysicsHandleComponent* PhysicsHandle = nullptr;
-	
+	UInputComponent* InputComponent = nullptr;
+	// Ray cast and grab what's in reach
+	void Grab();
+	// Called when grab is released
+	void Release();
 };
